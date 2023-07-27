472d484d2e26eff8f14cbb40c7028400fffac09d
Rahul Roy <91940155+RahulRoy-rsp@users.noreply.github.com>
2023-07-27 20:17:43 +0530

    confidential information

---
+++ dno.py
@@ -0,0 +1,21 @@
+# YOUR SAVED EMAIL LISTS
+email_list = {
+    'me': 'rahulroy74810gmail.com',
+    'roy': 'roysoftwareprogrammer@gmail.com',
+    'NAME OF PERSON': 'THIER EMAIL ADDRESS',
+}
+
+
+
+
+
+# YOUR TWITTER DEVELOPER ACCOUNT'S CREDENTIAL
+api_key = "API KEY"
+api_secret_key = "API SECRET KEY"
+bearer_token = "BEARER TOKEN"
+access_token = "ACCESS SECRET"
+access_token_secret = "ACCESS TOKEN SECRET"
+
+# YOUR EMAIL ADDRESS CREDENTIAL
+email_address = 'ENTER YOUR EMAIL ADDRESS'
+email_pwd = 'ENTER YOUR EMAIL-ID PASSWORD'
